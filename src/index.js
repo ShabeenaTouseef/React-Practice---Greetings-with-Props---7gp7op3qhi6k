967685582a861291652a9c344f45b3d21cba8dc0
ShabeenaTouseef <85632232+ShabeenaTouseef@users.noreply.github.com>
2021-12-10 15:58:22 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import Welcome from './Welcome.js';
 
 
 
-ReactDOM.render(<Welcome />, document.getElementById("root"));
+
+ReactDOM.render(<App />, document.getElementById("root"));
